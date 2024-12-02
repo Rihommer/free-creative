36576be1db4464461739bdca2b075423b937b4d2
Vadim Yurasov <barracuda89.811@gmail.com>
2024-12-02 19:17:32 +0400

    add Link

---
+++ src/components/Link.vue
@@ -0,0 +1,43 @@
+<script setup>
+    const link = defineProps(['text', 'href'])
+</script>
+
+<template>
+    <a class="link" :href="href">{{ text }}</a>
+</template>
+
+<style scoped>
+    .link {
+        display: inline-block;
+        padding: 13.5px 47px;
+
+        font-size: 1.5rem;
+        line-height: 21px;
+        color: var(--font-color);
+        text-decoration: none;
+
+        border: 1px solid #fff;
+        border-radius: 40px;
+        background-color: #fff;
+    }
+
+    .link--border {
+        border-color: #000;
+    }
+
+    @media(min-width: 768px) {
+        .link {
+            padding-block: 18px;
+
+            font-size: 1.6rem;
+            line-height: 1.4;
+        }
+    }
+
+    @media (min-width: 1200px) {
+        .link {
+            padding-block: 22px;
+            font-size: 1.8rem;
+        }
+    }
+</style>
