afc3149a860baeea90f6c6a32223c812f67004d1
Vadim Yurasov <barracuda89.811@gmail.com>
2024-12-03 17:09:04 +0400

    add Social

---
+++ src/components/Social.vue
@@ -0,0 +1,22 @@
+<script setup>
+    const social = defineProps(['href', 'src', 'alt']);
+</script>
+
+<template>
+    <a class="social" :href=href>
+        <img class="social__img" :src=src :alt=alt width="13" height="13">
+    </a>
+</template>
+
+<style scoped>
+.social {
+	display: grid;
+	place-content: center;
+
+	width: 36px;
+	aspect-ratio: 1;
+
+	border: 1px solid #fff;
+	border-radius: 50%;
+}
+</style>
